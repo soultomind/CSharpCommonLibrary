46d2e178a10e75a6ab177424cb5d26f2a1fb1236
soultomind <soultomind@naver.com>
2020-04-15 12:46:57 +0900

    단순 - 탭 없는 탭컨트롤 UI 클래스 추가

---
+++ CommonLibrary/UI/TablessControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonLibrary.UI
+{
+    public class TablessControl : TabControl
+    {
+        public const int TCM_ADJUSTRECT = 0x1328;
+
+        public bool IsTabless
+        {
+            get; set;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (IsTabless)
+            {
+                if ((m.Msg == TCM_ADJUSTRECT) && !DesignMode)
+                {
+                    m.Result = (IntPtr)1;
+                }
+                else
+                {
+                    base.WndProc(ref m);
+                }
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
