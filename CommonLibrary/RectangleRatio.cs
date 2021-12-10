13f67d9e83cf0f9b8016ae71624a9109885a86dd
soultomind <soultomind@naver.com>
2021-12-10 20:24:44 +0900

    단순 - 가로 세로 비율 구하는 클래스 추가

---
+++ CommonLibrary/RectangleRatio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 사각형 비율
+    /// </summary>
+    public class RectangleRatio
+    {
+        public int WidthRatio { get; set; }
+        public int HeightRatio { get; set; }
+        private RectangleRatio() { WidthRatio = HeightRatio = 0; }
+        private RectangleRatio(int widthRatio, int heightRatio)
+        {
+            WidthRatio = widthRatio;
+            HeightRatio = heightRatio;
+        }
+
+        public static RectangleRatio ToRectangleRatio(int width, int height)
+        {
+            if (!(width >= 0 && height >= 0))
+            {
+                throw new ArgumentException("");
+            }
+
+            int max = 0, min = 0, temp = 0, gcd = 0;
+            if (width < height)
+            {
+                max = height;
+                min = width;
+            }
+            else
+            {
+                max = width;
+                min = height;
+            }
+
+            while ((max % min) != 0)
+            {
+                temp = max % min;
+                max = min;
+                min = temp;
+            }
+
+            gcd = min;
+
+            int widthRatio = width / gcd;
+            int heightRatio = height / gcd;
+            return new RectangleRatio(widthRatio, heightRatio);
+        }
+    }
+}
