d5935a5fae67bf9a3d28af67791e7fb7cef9e367
soultomind <soultomind@naver.com>
2021-12-10 20:24:15 +0900

    단순 - 리팩토링 - Byte <-> Int 변환 메서드 추가

--- CommonLibrary/Utilities/ConvertUtility.cs
+++ CommonLibrary/Utilities/ConvertUtility.cs
@@ -34,36 +34,81 @@ namespace CommonLibrary.Utilities
             return Convert.FromBase64String(text);
         }
 
-        public static byte[] GetBytes(string text, string encodingName = "UTF-8")
+        public static Encoding GetEncoding(string encodingName = "UTF-8")
         {
             if (encodingName == null)
             {
-                throw new ArgumentNullException("EncodingName");
+                throw new ArgumentNullException(nameof(encodingName));
             }
 
             Encoding encoding = Encoding.GetEncoding(encodingName);
             if (encoding == null)
             {
-                throw new ArgumentNullException("Encoding");
+                throw new ArgumentNullException(nameof(encoding));
             }
 
+            return encoding;
+        }
+
+        public static byte[] GetBytes(string text, string encodingName = "UTF-8")
+        {
+            Encoding encoding = GetEncoding(encodingName);
             return encoding.GetBytes(text);
         }
 
         public static string GetString(byte[] bytes, string encodingName = "UTF-8")
         {
-            if (encodingName == null)
-            {
-                throw new ArgumentNullException("EncodingName");
-            }
-
-            Encoding encoding = Encoding.GetEncoding(encodingName);
-            if (encoding == null)
-            {
-                throw new ArgumentNullException("Encoding");
-            }
-
+            Encoding encoding = GetEncoding(encodingName);
             return encoding.GetString(bytes);
+        }
+
+        public static byte[] IntToByteArray(int value)
+        {
+            return BitConverter.GetBytes(value);
+            
+            // DeadCode
+            /*
+            if (useBitConverter)
+            {
+                return BitConverter.GetBytes(value);
+            }
+            else
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    byte[] byteArray = new byte[4];
+                    byteArray[3] = (byte)(value >> 24);
+                    byteArray[2] = (byte)(value >> 16);
+                    byteArray[1] = (byte)(value >> 8);
+                    byteArray[0] = (byte)(value);
+                    return byteArray;
+                }
+                else
+                {
+                    byte[] byteArray = new byte[4];
+                    byteArray[0] = (byte)(value >> 24);
+                    byteArray[1] = (byte)(value >> 16);
+                    byteArray[2] = (byte)(value >> 8);
+                    byteArray[3] = (byte)(value);
+                    return byteArray;
+                }
+            }
+            */
+        }
+
+        public static int ByteArrayToInt(byte[] value, int startIndex = 0)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (!(value.Length >= 4))
+            {
+                throw new ArgumentException(nameof(value) + ".length=" + value.Length);
+            }
+
+            return BitConverter.ToInt32(value, startIndex);
         }
     }
 }
