0d273b6eb0c9475c04c325f2597d1b0b915ad7a2
soultomind <soultomind@naver.com>
2021-12-10 20:23:47 +0900

    단순 - 아직 확실하지 않는 기능 메서드 제거

--- CommonLibrary/Utilities/ColorUtility.cs
+++ CommonLibrary/Utilities/ColorUtility.cs
@@ -18,50 +18,11 @@ namespace CommonLibrary.Utilities
             return Color.FromArgb(alapha, red, green, blue); ;
         }
 
-
-        public static bool IsValidCssStyleSharpRgba(string styleSharpRgba)
+        public static bool IsValidCssSharpRgba(string styleSharpRgba)
         {
+            // #[[:xdigit:]{8}] 테스트해보기
             Regex regex = new Regex("#[0-9A-Fa-f]{8}");
             return regex.IsMatch(styleSharpRgba);
-        }
-
-        public static int ToCssStyleRgbaNumber(string styleRgbaNumber)
-        {
-            if (styleRgbaNumber == null)
-            {
-                throw new ArgumentNullException("styleRgbaNumber");
-            }
-
-            if (styleRgbaNumber.Length == 0)
-            {
-                throw new ArgumentException("styleRgbaNumber.Length is zero");
-            }
-
-            if (!styleRgbaNumber.StartsWith("rgba("))
-            {
-                throw new ArgumentException("styleRgbaNumber is invalid value, usage is rgba(255,0,0,0)");
-            }
-
-            if (!styleRgbaNumber.EndsWith(")"))
-            {
-                throw new ArgumentException("styleRgbaNumber is invalid value, usage is rgba(255,0,0,0)");
-            }
-
-            styleRgbaNumber = styleRgbaNumber.Substring(5, styleRgbaNumber.Length - 6);
-            string[] arrRgba = styleRgbaNumber.Split(',');
-            if (arrRgba.Length != 4)
-            {
-                throw new ArgumentException("argb is invalid value, usage is rgba(255,0,0,0)");
-            }
-
-            int red = int.Parse(arrRgba[0]);
-            int green = int.Parse(arrRgba[1]);
-            int blue = int.Parse(arrRgba[2]);
-            int alpha = int.Parse(arrRgba[3]);
-            alpha = alpha << 24;
-            red = red << 16;
-            green = green << 8;
-            return alpha + red + green + blue;
         }
     }
 }
