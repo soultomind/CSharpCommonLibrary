08f55eb474471370525869cd30a77ab12e7ddf57
soultomind <soultomind@naver.com>
2022-01-02 14:03:00 +0900

    신규 - 스크린 이미지 캡쳐 기능 추가

---
+++ CSharpCommonLibrary/MultiScreenManager.cs
@@ -0,0 +1,90 @@
+﻿using CommonLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharpCommonLibrary
+{
+    public class MultiScreenManager
+    {
+        public const int InvalidScreenIndex = -1;
+        /// <summary>
+        /// 타겟이 될 <see cref="System.Windows.Forms.Screen"/> 인덱스
+        /// </summary>
+        public int TargetScreenIndex { get; set; }
+        /// <summary>
+        /// 타겟이 될 <see cref="System.Windows.Forms.Screen"/> 스크린 Bounds 사이즈
+        /// </summary>
+        public Size TargetResolutionDisplayBoundsSize { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetScreenIndex"></param>
+        /// <exception cref="System.InvalidOperationException"></exception>
+        public MultiScreenManager(int targetScreenIndex)
+        {
+            if (ScreenUtility.ScreenLength == 1)
+            {
+                throw new InvalidOperationException("Screen.AllScreens.Length==1");
+            }
+
+            if (!ScreenUtility.IsValidIndex(targetScreenIndex))
+            {
+                throw new ArgumentException(nameof(targetScreenIndex));
+            }
+
+            TargetScreenIndex = targetScreenIndex;
+            TargetResolutionDisplayBoundsSize = Size.Empty;
+        }
+
+        public MultiScreenManager(Size targetResolutionDisplayBoundsSize)
+        {
+            if (ScreenUtility.ScreenLength == 1)
+            {
+                throw new InvalidOperationException("Screen.AllScreens.Length==1");
+            }
+
+            if (!ScreenUtility.EqualsScreenBoundsSize(targetResolutionDisplayBoundsSize))
+            {
+                throw new ArgumentException(nameof(targetResolutionDisplayBoundsSize));
+            }
+
+            TargetResolutionDisplayBoundsSize = targetResolutionDisplayBoundsSize;
+            TargetScreenIndex = InvalidScreenIndex;
+        }
+
+        
+        public Screen TargetScreen
+        {
+            get
+            {
+                Screen screen = null;
+                if (TargetScreenIndex == InvalidScreenIndex)
+                {
+                    screen = ScreenUtility.GetTargetScreen(TargetResolutionDisplayBoundsSize);
+                }
+                else
+                {
+                    screen = ScreenUtility.GetTargetScreen(TargetScreenIndex);
+                }
+                return screen;
+            }
+        }
+
+        public Screen GetContainsPointScreenBounds(Point point)
+        {
+            return ScreenUtility.ContainsPointScreenBounds(point);
+        }
+
+        public Point CalcScreenBoundsInCenterPoint(Screen screen, Size size)
+        {
+            int x = (screen.Bounds.Size.Width - size.Width) / 2;
+            int y = (screen.Bounds.Size.Height - size.Height) / 2;
+            return new Point(screen.Bounds.Location.X + x, screen.Bounds.Location.Y + y);
+        }
+    }
+}
---
+++ CSharpCommonLibrary/ScreenImageCapture.cs
@@ -0,0 +1,128 @@
+﻿using CommonLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharpCommonLibrary
+{
+    public delegate void CreateScreenImageCaptureEventHandler(object sender, ScreenImageCaptureEventArgs e);
+    public class ScreenImageCaptureEventArgs : EventArgs
+    {
+        public Exception Exception { get; private set; }
+        public Bitmap Bitmap { get; private set; }
+
+        public ScreenImageCaptureEventArgs(Bitmap bitmap)
+        {
+            Bitmap = bitmap;
+        }
+
+        public ScreenImageCaptureEventArgs(Exception exception)
+        {
+            Exception = exception;
+        }
+    }
+    public class ScreenImageCapture
+    {
+        public const int InvalidScreenIndex = -1;
+
+        public event CreateScreenImageCaptureEventHandler CreateScreenImageCapture;
+        /// <summary>
+        /// <see cref="System.Windows.Forms.Screen"/> 인덱스
+        /// </summary>
+        public int ScreenIndex { get; set; }
+        public Size ResolutionDisplaySize { get; set; }
+
+        public int Interval { get; set; }
+        public bool IsStart { get; private set; }
+        private Timer _Timer;
+        public ScreenImageCapture(int screenIndex, int interval = 500)
+        {
+            if (!ScreenUtility.IsValidIndex(screenIndex))
+            {
+                throw new ArgumentException("Usage " + nameof(screenIndex) + "=0-" + (Screen.AllScreens.Length - 1));
+            }
+
+            if (!(interval >= 500 && interval <= 3000))
+            {
+                throw new ArgumentException("Usage " + nameof(interval) + "=500-3000");
+            }
+
+            ScreenIndex = screenIndex;
+            Interval = interval;
+        }
+
+        public ScreenImageCapture(Size resolutionDisplaySize, int interval = 500)
+        {
+            if (!ScreenUtility.EqualsScreenBoundsSize(resolutionDisplaySize))
+            {
+                throw new ArgumentException(nameof(resolutionDisplaySize));
+            }
+
+            if (!(interval >= 500 && interval <= 3000))
+            {
+                throw new ArgumentException("Usage " + nameof(interval) + "=500-3000");
+            }
+
+            ResolutionDisplaySize = resolutionDisplaySize;
+            Interval = interval;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap bitmap = null;
+
+                Rectangle bounds = Rectangle.Empty;
+                if (ScreenIndex == InvalidScreenIndex)
+                {
+                    bounds = new MultiScreenManager(ResolutionDisplaySize).TargetScreen.Bounds;
+                }
+                else
+                {
+                    bounds = new MultiScreenManager(ScreenIndex).TargetScreen.Bounds;
+                }
+
+                bitmap = new Bitmap(bounds.Width, bounds.Height);
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size);
+                }
+
+                CreateScreenImageCapture?.Invoke(this, new ScreenImageCaptureEventArgs(bitmap));
+            }
+            catch (Exception ex)
+            {
+                CreateScreenImageCapture?.Invoke(this, new ScreenImageCaptureEventArgs(ex));
+            }
+        }
+
+        public void Close()
+        {
+            Stop();
+            _Timer = null;
+        }
+
+        public void Start()
+        {
+            if (_Timer == null)
+            {
+                _Timer = new Timer();
+                _Timer.Interval = Interval;
+                _Timer.Tick += Timer_Tick;
+            }
+
+            _Timer.Start();
+        }
+
+        public void Stop()
+        {
+            if (_Timer != null)
+            {
+                _Timer.Stop();
+            }
+        }
+    }
+}
