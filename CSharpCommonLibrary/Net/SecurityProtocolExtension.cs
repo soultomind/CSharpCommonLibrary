b6b783fe8d9a31f0a372e1a86d260c2e6bd0e4c2
soultomind <soultomind@naver.com>
2022-01-25 21:54:22 +0900

    신규 - .NETFramework 3.5 용 Tls12

---
+++ CSharpCommonLibrary/Net/SecurityProtocolExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Security.Authentication;
+
+namespace CommonLibrary.Net
+{
+    /// <summary>
+    /// <para>.NET Framework 3.5 Supported </para>
+    /// <see cref="System.Net.ServicePointManager.SecurityProtocol"/> 확장 클래스
+    /// </summary>
+    public class SecurityProtocolExtension
+    {
+        /// <summary>
+        /// Tls1.1 SslProtocols
+        /// </summary>
+        public const SslProtocols _Tls11 = (SslProtocols)0x00000300;
+
+        /// <summary>
+        /// Tls1.2 SslProtocols
+        /// </summary>
+        public const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+
+        /// <summary>
+        /// Tls1.1 SecurityProtocolType
+        /// </summary>
+        public const SecurityProtocolType Tls11 = (SecurityProtocolType)_Tls11;
+
+        /// <summary>
+        /// Tls1.2 SecurityProtocolType
+        /// </summary>
+        public const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+
+        /// <summary>
+        /// Tls12 설정을 합니다.
+        /// </summary>
+        public static void SetTls12()
+        {
+            SecurityProtocolType type =
+                ServicePointManager.SecurityProtocol |
+                SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
+                Tls11 | Tls12;
+
+            ServicePointManager.SecurityProtocol = type;
+        }
+    }
+}
